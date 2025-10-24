f4f5b2114bd84fa1dae281cf8e1c856733c8459d
Placids <yenc62518@gmail.com>
2025-10-25 06:39:46 +0700

    Thêm find_lr() ở backbone/model_class

--- DEEP LEARNING/Custom_Dataset/logger.py
+++ DEEP LEARNING/Custom_Dataset/logger.py
@@ -13,15 +13,18 @@ trainer: chạy trên auto accelerator, tối đa 10 epoch, sử dụng:
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 import pytorch_lightning as PL
-def trainer():
+from pytorch_lightning.tuner import Tuner
+def trainer(MAX_EPOCHS = 10):
     logger = TensorBoardLogger(save_dir='tb_log')
     checkpoint = ModelCheckpoint(monitor='val_loss', mode = 'min', save_top_k=1, save_weights_only=False)
     earlystop = EarlyStopping(monitor='val_f1', mode = 'max', min_delta = 0.005, patience=3)
     trainer = PL.Trainer(accelerator='auto',
                         logger = logger,
                         callbacks = [checkpoint, earlystop],
-                        max_epochs=10)
+                        max_epochs=MAX_EPOCHS)
     return trainer
-# tuner = Tuner(trainer)
-# lr_finder = tuner.lr_find(model, datamodule=datamodule, min_lr = 0.001, max_lr = 0.01)
-# model.hparams.lr = lr_finder.suggestion()
+def find_lr(model, datamodule):
+    tuner = Tuner(trainer)
+    lr_finder = tuner.lr_find(model, datamodule=datamodule, min_lr = 0.001, max_lr = 0.01)
+    return lr_finder.suggestion()
+
--- DEEP LEARNING/Custom_Dataset/train.py
+++ DEEP LEARNING/Custom_Dataset/train.py
@@ -1,8 +1,16 @@
 from backbones import model_class
 from dataloader import dataprocess
 import logger
+MAX_EPOCHS = 10
+INPUT_SIZE = 3
+NUM_CLASS = 3
+KERNEL = 10
+LEARNING_RATE = 0.003
+
 if __name__ == '__main__':
-    trainer = logger.trainer()
-    model = model_class.TinyVGG(3,3,10,0.003)
-    datamodule = dataprocess.DataModule(batch_size=32) 
+    trainer = logger.trainer(MAX_EPOCHS)
+    model = model_class.TinyVGG(INPUT_SIZE,NUM_CLASS,KERNEL,LEARNING_RATE)
+    datamodule = dataprocess.DataModule(batch_size=32)
+    model.hparams.lr = logger.find_lr(model, datamodule)
     trainer.fit(model, datamodule=datamodule)
+    
